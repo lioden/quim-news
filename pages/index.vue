32a7f162d15a9b0987126ff909547a6085fb7ed1
Edo <saculdasilva@hotmail.com>
2019-03-28 18:03:32 +0000

    Section Notícias Recentes added

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <v-container fluid grid-list-lg fill-height>
+      <v-flex xs3>
+        <v-card>
+          <v-img
+            src="https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            height="300"
+          ></v-img>
+        </v-card>
+      </v-flex>
+    </v-container>
     <tabsverticais />
     <v-container fluid grid-list-md>
       <v-layout row wrap>
@@ -51,7 +61,11 @@
         </v-flex>
       </v-layout>
     </v-container>
-    <tabsverticais />
+    <p class="font-weight-bold display-1" style="padding-left:10px">
+      <bold>Notícias Recentes</bold>
+    </p>
+    <hr />
+    <tabsverticais style="padding-top:20px" />
     <row4cards />
     <minicards />
   </div>
@@ -68,7 +82,10 @@ export default {
     carro,
     row4cards,
     minicards
-  }
+  },
+  data: () => ({
+    show: false
+  })
 }
 </script>
 
