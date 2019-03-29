2c094c861101f4d7924a47a0244219e310a4a3a4
Edo <saculdasilva@hotmail.com>
2019-03-29 15:53:41 +0000

    -Layout built from scratch, was too buggy, examples on Inspire Page
    -Grid system working
    -New Section
    -Multiple new components inside section
    To do: Detach components from sectioncards-one to separate components to call them inside each grid element on Inspire

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,14 +1,61 @@
 <template>
   <div>
-    <kitcards />
+    <v-container fluid grid-list-lg>
+      <v-layout row wrap>
+        <v-flex d-flex xs8>
+          <v-layout row wrap>
+            <v-flex d-flex xs8>
+              <v-card color="indigo lighten-2" dark tile flat>
+                card1
+              </v-card>
+            </v-flex>
+            <v-flex d-flex xs4>
+              <v-card color="indigo lighten-2" dark tile flat>
+                card2
+              </v-card>
+            </v-flex>
+            <v-flex d-flex xs4>
+              <v-card color="yellow lighten-2" dark tile flat>
+                card3
+              </v-card>
+            </v-flex>
+            <v-flex d-flex xs8>
+              <v-card color="yellow lighten-2" dark tile flat>
+                card4
+              </v-card>
+            </v-flex>
+            <v-flex d-flex xs12>
+              <v-card color="black lighten-2" dark tile flat>
+                card5
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex d-flex xs4>
+          <v-card color="red lighten-2" dark tile flat>
+            Latest News
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <div>
+      <cardsone />
+      <cardsone />
+    </div>
+    <div>
+      <cardsone />
+    </div>
   </div>
 </template>
 
 <script>
-import kitcards from '~/components/kitcards'
+import cardsone from '~/components/cardsection-one'
 export default {
   components: {
-    kitcards
-  }
+    cardsone
+  },
+  data: () => ({
+    lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
+  })
 }
 </script>
