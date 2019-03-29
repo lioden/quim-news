09517b993d8e118dab4bf3bcc7223d8935309a01
Edo <saculdasilva@hotmail.com>
2019-03-29 17:56:43 +0000

    Added carousel to inspire/desporto page

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -5,22 +5,17 @@
         <v-flex d-flex xs12 md8>
           <v-layout row wrap>
             <v-flex d-flex xs12 md8>
-              <v-card color="red lighten-2" dark height="400">
+              <v-card color="red lighten-2" dark>
                 <v-img
-                  style="min-height:250px;max-height:200px"
+                  style="min-height:250px;max-height:350px"
                   src="https://images.unsplash.com/photo-1553643182-15d168e4d680?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
                 ></v-img>
                 <v-card-text>{{ lorem.slice(0, 40) }}</v-card-text>
               </v-card>
             </v-flex>
             <v-flex d-flex xs12 md4>
-              <v-card color="grey darken-3" dark height="400">
-                <v-img
-                  :src="one"
-                  style="min-height:200px;max-height:200px"
-                ></v-img>
-                <v-card-title primary class="title">Lorem</v-card-title>
-                <v-card-text>{{ lorem.slice(0, 250) }}</v-card-text>
+              <v-card color="grey darken-3" dark>
+                <carro></carro>
               </v-card>
             </v-flex>
             <v-flex d-flex xs12 md6>
@@ -69,10 +64,12 @@
 <script>
 import latestnews from '~/components/latestnews'
 import cardlandscape from '~/components/card-landscape'
+import carro from '~/components/carro'
 export default {
   components: {
     latestnews,
-    cardlandscape
+    cardlandscape,
+    carro
   },
   data: () => ({
     lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`,
